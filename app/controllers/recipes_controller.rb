8afa18cd88ebaa1a5d79fde3f3c4a47ca39a9c34
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-05 19:17:00 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -27,5 +27,5 @@ class RecipesController < ApplicationController
 
     def recipe_params
       params.require(:recipe).permit(:title, :ingredients_attributes=>[:name, :quantity])
-    end 
+    end
 end
